3aa8f69d9abedeb5203a0ecda04532686120b171
Kurt Hetrick <khetric1@jhmi.edu>
2018-06-22 23:08:52 -0400

    added functionality to create pvcf of 300 samples chunks for each bed file split. commented out the rest for the moment

--- STD_VQSR/A01_COMBINE_GVCF.sh
+++ STD_VQSR/A01_COMBINE_GVCF.sh
@@ -40,19 +40,22 @@ echo
 
 	CORE_PATH=$4
 	PROJECT_MS=$5
-	GVCF_LIST=$6
+	PGVCF_LIST=$6
 	PREFIX=$7
 	BED_FILE_NAME=$8
 
+PGVCF_LIST_NAME=$(basename $PGVCF_LIST .list)
+
 START_COMBINE_GVCF=`date '+%s'`
 
-	CMD=$JAVA_1_8'/java -jar'
+	CMD=$JAVA_1_8'/java'
+	CMD=$CMD' -jar'
 	CMD=$CMD' '$GATK_DIR'/GenomeAnalysisTK.jar'
 	CMD=$CMD' -T CombineGVCFs'
 	CMD=$CMD' --disable_auto_index_creation_and_locking_when_reading_rods'
 	CMD=$CMD' -R '$REF_GENOME
-	CMD=$CMD' --variant '$GVCF_LIST
-	CMD=$CMD' -o '$CORE_PATH'/'$PROJECT_MS'/GVCF/AGGREGATE/'$PREFIX'.'$BED_FILE_NAME'.g.vcf.gz'
+	CMD=$CMD' --variant '$PGVCF_LIST
+	CMD=$CMD' -o '$CORE_PATH'/'$PROJECT_MS'/GVCF/AGGREGATE/'$PGVCF_LIST_NAME'.'$PREFIX'.'$BED_FILE_NAME'.g.vcf.gz'
 	CMD=$CMD' -L '$CORE_PATH/$PROJECT_MS/TEMP/BED_FILE_SPLIT/$BED_FILE_NAME'.bed'
 	CMD=$CMD' --annotation AS_BaseQualityRankSumTest'
 	CMD=$CMD' --annotation AS_FisherStrand'
@@ -80,4 +83,4 @@ echo $PROJECT_MS",A01,COMBINE_GVCF,"$HOSTNAME","$START_COMBINE_GVCF","$END_COMBI
 # check to see if the index is generated which should send an non-zero exit signal if not.
 # eventually, will want to check the exit signal above and push out whatever it is at the end. Not doing that today though.
 
-ls $CORE_PATH/$PROJECT_MS/GVCF/AGGREGATE/$PREFIX"."$BED_FILE_NAME".g.vcf.gz.tbi"
+ls $CORE_PATH/$PROJECT_MS/GVCF/AGGREGATE/$PGVCF_LIST_NAME"."$PREFIX"."$BED_FILE_NAME".g.vcf.gz.tbi"
